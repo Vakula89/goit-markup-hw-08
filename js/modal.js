6e7ac339da680ee100d75329ba4013ee91db6c81
Vakula89 <vasyl.yento@gmail.com>
2022-05-21 17:45:13 +0300

    hw-8-2

--- js/modal.js
+++ js/modal.js
@@ -3,22 +3,14 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    mobileMenuOpenBtn: document.querySelector('[data-menu-open]'),
-    mobileMenuCloseBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  refs.mobileMenuOpenBtn.addEventListener('click', toggleMenu);
-  refs.mobileMenuCloseBtn.addEventListener('click', toggleMenu);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
-
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden")
-  }
+  
 })();
